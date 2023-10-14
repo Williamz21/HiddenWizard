1226f65e762fc4bfedc9b21a490d5c62499f08e1
Hamtony <hamtony2604@gmail.com>
2023-10-14 18:04:44 -0500

    AAAAAAA
    no

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
     private float dirX = 0;
     private float dirY = 0;
-    private enum movemntStatemnt {idle, left, rigth, up, down}
+    private enum movemntStatemnt {idle, left, rigth, up, down, upleft, uprigth}
     public SpriteRenderer spriteRend;
     /*public Projectile head;*/
 
@@ -22,26 +22,27 @@ public class PlayerController : MonoBehaviour
     }
 
     void updateAnim(){
-        movemntStatemnt state;
-        if(dirX > 0f){
-            state = movemntStatemnt.rigth;
-            spriteRend.flipX = false;
+        movemntStatemnt state = 0;
+        if(dirX == 0 && dirY == 0){
+            state = movemntStatemnt.idle;
         }
-        else if(dirX < 0f){
+        else if(dirX > 0f && dirY > 0f){
+            state = movemntStatemnt.uprigth;
+        }
+        else if(dirX < 0f && dirY > 0f){
+            state = movemntStatemnt.upleft;
+        }
+        else if(dirX > 0f && dirY >= 0f){
+            state = movemntStatemnt.rigth;
+        }
+        else if(dirX < 0f && dirY <= 0f){
             state = movemntStatemnt.left;
-            spriteRend.flipX = true;
         }
         else if(dirY < 0f){
-            spriteRend.flipX = false;
             state = movemntStatemnt.down;
         }
         else if(dirY > 0f){
-            spriteRend.flipX = false;
-            state = movemntStatemnt.up;
-        }
-        else{
-            spriteRend.flipX = false;
-            state = movemntStatemnt.idle;
+            state = movemntStatemnt.up; 
         }
         animator.SetInteger("state", (int)state);
     }
